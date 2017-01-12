20d46edfde5667819ea4b359986365ea6e131f71
zach-col <theperfectpicture100@gmail.com>
2017-01-12 11:23:08 -0600

    updated to new navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,9 @@
-      $(".navbar-nav li a").click(function(event){
-          if (!$(this).parent().hasClass('dropdown'))
-            $(".navbar-collapse").collapse('hide');
-      });
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
