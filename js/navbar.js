ccfd32c6dd1773de84a7966196fa94e2c334baef
zach-col <theperfectpicture100@gmail.com>
2017-01-28 17:53:01 -0600

    fixed navbar bug

--- js/navbar.js
+++ js/navbar.js
@@ -9,4 +9,5 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    $('body,html').css('overflow','scroll');
 }
